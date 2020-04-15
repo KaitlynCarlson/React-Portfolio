539d419d67fc1c723d180cd2a77554610fcda3bd
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-04-15 11:49:39 -0700

    Cat component added and rendering

---
+++ src/components/cat/cat.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import cat from "./cat3.png";
+function Cat() {
+  return <img alt="cat-icon" src={cat} height="50px" />;
+}
+export default Cat;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Navigation from "../components/navigation/navigation";
+import Cat from "../components/cat/cat";
 import "./style.css";
 const Home = () => (
   <div>
     <div id="wrapper">
       <Navigation />
+      <div className="container" id="content-container">
+        <Cat />
+      </div>
     </div>
   </div>
 );
