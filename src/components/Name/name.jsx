ecfac7d369c63c397b5f37dad87ca617406a79cc
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-04-15 12:11:46 -0700

    Added Name component

---
+++ src/components/Name/name.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+function Name() {
+  return (
+    <div>
+      <h1 className="display-4 name">Kaitlyn Carlson</h1>
+      <h3 className="lead name">Full Stack Web Developer</h3>
+    </div>
+  );
+}
+export default Name;
--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -1,41 +1,42 @@
 import React from "react";
+import Name from "../Name/name";
 import "./style.css";
 function Navigation() {
   return (
-    <nav id="nav" class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">
-        Kaitlyn Carlson
-      </a>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              About
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              Portfolio
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              Skills
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              Resume
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              Contact
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <div id="navigation-container">
+      <Name />
+      <nav id="nav" class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                About
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Portfolio
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Skills
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Resume
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   );
 }
 export default Navigation;
