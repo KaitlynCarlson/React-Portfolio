6b62863ab21d5190cbfa5cbd94a5877cccfdf09f
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-04-17 11:35:16 -0700

    conditional page rendering now working

--- src/components/displayCard/displayCard.jsx
+++ src/components/displayCard/displayCard.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import "./style.css";
-// import About from "../about/about";
-// import Contact from "../contact/contact";
-// import Skills from "../skills/skills";
-// import Projects from "../projects/projects";
+import About from "../about/about";
+import Contact from "../contact/contact";
+import Skills from "../skills/skills";
+import Projects from "../projects/projects";
 import Resume from "../resume/resume";
 class DisplayCard extends React.Component {
   render() {
-    return (
-      <React.Fragment>
-        {/* <About />
-        <Skills />
-        <Contact /> */}
-        {/* <Projects /> */}
-        <Resume />
-      </React.Fragment>
-    );
+    let render;
+    if (this.props.name === "about") {
+      return (render = <About />);
+    } else if (this.props.name === "contact") {
+      return (render = <Contact />);
+    } else if (this.props.name === "skills") {
+      return (render = <Skills />);
+    } else if (this.props.name === "projects") {
+      return (render = <Projects />);
+    } else if (this.props.name === "resume") {
+      return (render = <Resume />);
+    }
+    return <React.Fragment>{render}</React.Fragment>;
   }
 }
 export default DisplayCard;
