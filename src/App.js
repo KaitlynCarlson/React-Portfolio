2124bd0369d6373cc64f350be5d6da47b7c002ba
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-04-17 14:10:25 -0700

    Removed unnecessary react files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Home from "./pages/home";
-import "./App.css";
 
 function App() {
   return (
