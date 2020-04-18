056d5f075b12ef38f352189519b942f41f83dab2
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-04-17 19:49:02 -0700

    Deploy:

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Home from "./pages/home";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
