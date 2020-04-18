bfa79d467abf005a30bfb62ddcc69b82a06f1301
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-04-17 19:47:40 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Portfolio/precache-manifest.e49b95ad83d27676490595c0240367ea.js"
+  "/React-Portfolio/precache-manifest.bc0772d9c5ddc324ef0054133660fa7f.js"
 );
 
 self.addEventListener('message', (event) => {
