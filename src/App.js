471d99b8bc8c46b7bcc808f897dd0ba1bf2030ed
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-04-19 12:53:30 -0700

    Added router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import Home from "./pages/home";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/React-Portfolio" component={Home} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
