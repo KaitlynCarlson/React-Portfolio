8dec847c8c35a5cd30976694d1fddff5dc81e44b
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-05-23 00:19:15 -0700

    Resume download

--- src/components/resume/resume.jsx
+++ src/components/resume/resume.jsx
@@ -4,7 +4,7 @@ function Resume() {
   return (
     <div>
       <a
-        href="./KaitlynCarlson.pdf"
+        href="https://github.com/KaitlynCarlson/React-Portfolio/raw/master/src/components/resume/KaitlynCarlson.pdf"
         download
         className="btn btn-primary btn-xl d-flex"
         id="download-resume-anchor"
