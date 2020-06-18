9903f32c61ec0a46e022ad67f3f069d6fbc00a46
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-06-18 15:49:28 -0700

    changed header title

--- src/components/Name/name.jsx
+++ src/components/Name/name.jsx
@@ -4,7 +4,7 @@ function Name() {
   return (
     <div>
       <h1 className="display-4 name">Kaitlyn Anne Carlson</h1>
-      <h3 className="lead name">Software Engineer</h3>
+      <h3 className="lead name">Web Developer</h3>
     </div>
   );
 }
